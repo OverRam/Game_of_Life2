1a46ebaa4219e65aa9d7d90b5ccee4330eb69023
Dominik <77927514+OverRam@users.noreply.github.com>
2021-09-18 14:50:08 +0200

    Edit main to use pattern MCV

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        World world = new World(sc.nextInt(), sc.nextInt());
-        world.showWorld();
+        int sizeWorld = sc.nextInt();
+        long seed = sc.nextLong();
+        int generations = sc.nextInt();
+
+        WorldModel worldModel = new WorldModel(sizeWorld, seed);
+        worldModel.initialFillWorld();
+        WorldView worldView = new WorldView();
+        WorldController worldController = new WorldController(worldView, worldModel);
+
+        for (int i = 0; i < generations; i++) {
+            worldModel.setWorldModel(GenerationsWorlds.nextGeneration(worldModel).getWordMatrix(), worldModel.getSize());
+        }
+        worldController.updateView();
     }
 }
