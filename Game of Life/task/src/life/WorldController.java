ba2232086f0f01084bcc580023d8ad9a8cf60287
Dominik <77927514+OverRam@users.noreply.github.com>
2021-10-17 20:53:30 +0200

    Implements methods: waitMilliSec, clearScr, worldsGenerations, updateView

--- Game of Life/task/src/life/WorldController.java
+++ Game of Life/task/src/life/WorldController.java
@@ -1,16 +1,52 @@
 package life;
 
-public class WorldController {
-    WorldView worldView;
-    WorldModel worldModel;
+import java.io.IOException;
 
-    public WorldController(WorldView worldView, WorldModel worldModel) {
-        this.worldView = worldView;
+public class WorldController {
+    private final GameOfLife gameOfLifeView;
+    private final WorldModel worldModel;
+    private int generation = 0;
+
+    WorldController(GameOfLife gameOfLifeView, WorldModel worldModel) {
+        this.gameOfLifeView = gameOfLifeView;
         this.worldModel = worldModel;
     }
 
+    private static void waitMilliSec() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-    void updateView(){
-        worldView.printModel(worldModel);
+    private static void clearScr() {
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException ignored) {
+        }
+    }
+
+    void worldsGenerations(int NumberOfGenerations) {
+        for (int i = 0; i < NumberOfGenerations + 1; i++) {
+            NextGenerationsWorld.nextGenerationWorld(worldModel);
+            updateView();
+            waitMilliSec();
+            generation++;
+        }
+        generation = 0;
+    }
+
+    void updateView() {
+        gameOfLifeView.setNumGenerationLabel(generation);
+        gameOfLifeView.setNumAliveCells(worldModel.getLiveCells());
+
+        DrawWorld worldMap = gameOfLifeView.getDrawWorld();
+        worldMap.setWorldModel(worldModel);
+        worldMap.repaint();
+        gameOfLifeView.setDrawWorld(worldMap);
     }
 }
