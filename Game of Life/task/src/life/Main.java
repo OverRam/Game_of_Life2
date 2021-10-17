eaa315a6e6a064b4653902b6a19c652e1860f9dd
Dominik <77927514+OverRam@users.noreply.github.com>
2021-10-17 20:54:30 +0200

    Simplifying the main method

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -4,19 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int sizeWorld = sc.nextInt();
-        long seed = sc.nextLong();
-        int generations = sc.nextInt();
 
-        WorldModel worldModel = new WorldModel(sizeWorld, seed);
-        worldModel.initialFillWorld();
-        WorldView worldView = new WorldView();
-        WorldController worldController = new WorldController(worldView, worldModel);
+//        Scanner sc = new Scanner(System.in);
+//        int sizeWorld = sc.nextInt();
 
-        for (int i = 0; i < generations; i++) {
-            worldModel.setWorldModel(GenerationsWorlds.nextGeneration(worldModel).getWordMatrix(), worldModel.getSize());
-        }
-        worldController.updateView();
+
+        WorldController worldController = new WorldController(new GameOfLife(), new WorldModel(10));
+
+        worldController.worldsGenerations(10);
     }
 }
