9277256ee102a9cec76bbae3f04bea0928deb6c6
Dominik <77927514+OverRam@users.noreply.github.com>
2021-10-26 01:30:03 +0200

    Modify main method

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,16 +1,8 @@
 package life;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-//        Scanner sc = new Scanner(System.in);
-//        int sizeWorld = sc.nextInt();
-
-
-        WorldController worldController = new WorldController(new GameOfLife(), new WorldModel(10));
-
-        worldController.worldsGenerations(10);
+        GameOfLife game = new GameOfLife();
+//        game.run();
     }
 }
